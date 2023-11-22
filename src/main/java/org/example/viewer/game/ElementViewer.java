201aaa5d8a48867925167ff68daa67c10a5c53dd
Paulo Fidalgo <paulofidalgo@PauloFidalgo.Home>
2023-11-22 22:56:22 +0000

    Corrected Element Viewer

--- src/main/java/org/example/viewer/game/ElementViewer.java
+++ src/main/java/org/example/viewer/game/ElementViewer.java
@@ -2,6 +2,7 @@ package org.example.viewer.game;
 
 import org.example.GUI.GUI;
 import org.example.model.game.elements.Element;
+import org.example.viewer.Viewer;
 
 public interface ElementViewer<T extends Element> {
     void draw(T element, GUI gui);
