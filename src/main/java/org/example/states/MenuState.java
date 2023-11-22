8bc206015719ecd434cd1f3c295a48558ae9b711
Paulo Fidalgo <paulofidalgo@PauloFidalgo.home>
2023-11-22 15:50:47 +0000

    Added some states

---
+++ src/main/java/org/example/states/MenuState.java
@@ -0,0 +1,21 @@
+package org.example.states;
+
+import org.example.controller.Controller;
+import org.example.controller.menu.MenuController;
+import org.example.model.menu.Menu;
+
+public class MenuState extends State<Menu> {
+    public MenuState(Menu model) {
+        super(model);
+    }
+
+    @Override
+    protected Viewer<Menu> getViewer() {
+        return new MenuViewer(getModel());
+    }
+
+    @Override
+    protected Controller<Menu> getController() {
+        return new MenuController(getModel());
+    }
+}
