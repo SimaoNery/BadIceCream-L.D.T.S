1f9579121e6428670adc71e0dc50340d2f654b9e
Paulo Fidalgo <paulofidalgo@PauloFidalgo.home>
2023-11-22 16:11:09 +0000

    Created Menu base class and derived classes MainMenu and PauseMenu

--- src/main/java/org/example/model/menu/MainMenu.java
+++ src/main/java/org/example/model/menu/MainMenu.java
@@ -3,43 +3,10 @@ package org.example.model.menu;
 import java.util.Arrays;
 import java.util.List;
 
-public class Menu {
-    private final List<String> entries;
-    private int currentEntry = 0;
+public class MainMenu extends Menu {
 
-    public Menu() {
-        this.entries = Arrays.asList("Start", "Exit");
+    public MainMenu() {
+        super(Arrays.asList("Start", "Exit"));
     }
 
-    public void nextEntry() {
-        currentEntry++;
-        if (currentEntry > this.entries.size() - 1)
-            currentEntry = 0;
-    }
-
-    public void previousEntry() {
-        currentEntry--;
-        if (currentEntry < 0)
-            currentEntry = this.entries.size() - 1;
-    }
-
-    public String getEntry(int i) {
-        return entries.get(i);
-    }
-
-    public boolean isSelected(int i) {
-        return currentEntry == i;
-    }
-
-    public boolean isSelectedExit() {
-        return isSelected(1);
-    }
-
-    public boolean isSelectedStart() {
-        return isSelected(0);
-    }
-
-    public int getNumberEntries() {
-        return this.entries.size();
-    }
 }
---
+++ src/main/java/org/example/model/menu/PauseMenu.java
@@ -0,0 +1,9 @@
+package org.example.model.menu;
+
+import java.util.Arrays;
+
+public class PauseMenu extends Menu {
+    public PauseMenu() {
+        super(Arrays.asList("Resume", "Quit Game"));
+    }
+}
