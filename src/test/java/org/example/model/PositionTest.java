2573b3e1610a9656b0fbecac54e072c524585f92
nelsonneto21 <nelsonjoneto@gmail.com>
2023-11-23 18:03:42 +0000

    Created tests for Position

---
+++ src/test/java/org/example/model/PositionTest.java
@@ -0,0 +1,44 @@
+package org.example.model;
+
+import org.example.model.game.arena.Arena;
+import org.example.model.game.elements.IceCream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PositionTest {
+    private Position position;
+
+    @BeforeEach
+    void setUp() {
+        position = new Position(0,0);
+    }
+
+    @Test
+    public void testGetLeft() {
+        Position newpos = position.getLeft();
+        Position expected = new Position(-1, 0);
+        assertEquals(expected, newpos);
+    }
+    @Test
+    public void testGetRight() {
+        Position newpos = position.getRight();
+        Position expected = new Position(1, 0);
+        assertEquals(expected, newpos);
+    }
+    @Test
+    public void testGetUp() {
+        Position newpos = position.getUp();
+        Position expected = new Position(0, -1);
+        assertEquals(expected, newpos);
+    }
+    @Test
+    public void testGetDown() {
+        Position newpos = position.getDown();
+        Position expected = new Position(0, 1);
+        assertEquals(expected, newpos);
+    }
+}
