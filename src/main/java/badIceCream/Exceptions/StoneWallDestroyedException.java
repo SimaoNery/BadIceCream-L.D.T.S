d0faf33bb01067e16c2a0a85aebd97c2ca29ee37
Paulo Fidalgo <paulofidalgo@PauloFidalgo.home>
2023-11-29 15:58:41 +0000

    Created exception to be thrown when someone tries to destroy a stone wall

---
+++ src/main/java/badIceCream/Exceptions/StoneWallDestroyedException.java
@@ -0,0 +1,7 @@
+package badIceCream.Exceptions;
+
+public class StoneWallDestroyedException extends Exception {
+    public StoneWallDestroyedException() {
+        super("The stone wall has been destroyed.");
+    }
+}
