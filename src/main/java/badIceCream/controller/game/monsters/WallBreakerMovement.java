679ba4efe69cd2cd00ce8285f1c3dc646ffca320
Paulo Fidalgo <paulofidalgo@PauloFidalgo.home>
2023-11-29 16:00:16 +0000

    Created WallBreakerMovement

---
+++ src/main/java/badIceCream/controller/game/monsters/WallBreakerMovement.java
@@ -0,0 +1,47 @@
+package badIceCream.controller.game.monsters;
+
+import badIceCream.Exceptions.StoneWallDestroyedException;
+import badIceCream.controller.game.Step;
+import badIceCream.model.Position;
+import badIceCream.model.game.arena.Arena;
+import badIceCream.model.game.elements.monsters.Monster;
+
+import java.io.IOException;
+import java.util.List;
+
+public class WallBreakerMovement implements Step {
+    @Override
+    public void step(Monster monster, Arena arena) throws IOException {
+        Position pos = getPossible(monster, arena);
+        if (pos != null) {
+            if (arena.isIceWall(pos)) {
+                try {
+                    arena.iceWallDestroyed(pos);
+                }
+                catch (StoneWallDestroyedException e) {
+                    System.err.println("Error: " + e.getMessage());
+                }
+            }
+            moveMonster(monster, pos, arena);
+        }
+    }
+
+    private Position getPossible(Monster monster, Arena arena) {
+        List<Position> options = new java.util.ArrayList<>(List.of(new Position[]{monster.getPosition().getDown(), monster.getPosition().getLeft(), monster.getPosition().getUp(), monster.getPosition().getRight()}));
+
+        options.removeIf(pos -> !arena.isEmptyNoStoneWall(pos));
+
+        if (options.isEmpty()) return null;
+
+        java.util.Random random = new java.util.Random();
+        int randomIndex = random.nextInt(options.size());
+
+        return options.get(randomIndex);
+    }
+    @Override
+    public void moveMonster(Monster monster, Position position, Arena arena) {
+        monster.setPosition(position);
+        if (arena.getIceCream().getPosition().equals(position))
+            arena.getIceCream().changeAlive();
+    }
+}
