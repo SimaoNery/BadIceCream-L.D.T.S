dc084e2dacd4482857435ba8fe45d3ca61af6c3a
nelsonneto21 <nelsonjoneto@gmail.com>
2023-11-29 21:41:00 +0000

    Implemented auxiliary classes with methods to find shortest path to iceCream

--- src/main/java/badIceCream/controller/game/monsters/RunnerMovementEnabled.java
+++ src/main/java/badIceCream/controller/game/monsters/RunnerMovementEnabled.java
@@ -4,9 +4,11 @@ import badIceCream.controller.game.Step;
 import badIceCream.model.Position;
 import badIceCream.model.game.arena.Arena;
 import badIceCream.model.game.elements.monsters.Monster;
+import badIceCream.utils.ShortestPathNextMove;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Queue;
 
 public class RunnerMovementEnabled implements Step {
     @Override
@@ -16,18 +18,21 @@ public class RunnerMovementEnabled implements Step {
             if (pos != null) moveMonster(monster, pos, arena);
         }
     }
-
     private Position getPossible(Monster monster, Arena arena) {
-        List<Position> options = new java.util.ArrayList<>(List.of(new Position[]{monster.getPosition().getDown(), monster.getPosition().getLeft(), monster.getPosition().getUp(), monster.getPosition().getRight()}));
+        Position nexMove = ShortestPathNextMove.findShortestPath(monster, arena);
+        if (nexMove.equals(new Position(-1, -1))) {
+            List<Position> options = new java.util.ArrayList<>(List.of(new Position[]{monster.getPosition().getDown(), monster.getPosition().getLeft(), monster.getPosition().getUp(), monster.getPosition().getRight()}));
 
-        options.removeIf(pos -> !arena.isEmptyMonsters(pos));
+            options.removeIf(pos -> !arena.isEmptyMonsters(pos));
 
-        if (options.isEmpty()) return null;
+            if (options.isEmpty()) return null;
 
-        java.util.Random random = new java.util.Random();
-        int randomIndex = random.nextInt(options.size());
+            java.util.Random random = new java.util.Random();
+            int randomIndex = random.nextInt(options.size());
 
-        return options.get(randomIndex);
+            return options.get(randomIndex);
+        }
+        return nexMove;
     }
     @Override
     public void moveMonster(Monster monster, Position position, Arena arena) {
