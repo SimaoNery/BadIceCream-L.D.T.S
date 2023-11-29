adb083c55e1f41c1bc2044e0c3c2ac296a5b438d
Paulo Fidalgo <paulofidalgo@PauloFidalgo.home>
2023-11-29 15:59:52 +0000

    Created RunnerMovement (not complete)

---
+++ src/main/java/badIceCream/controller/game/monsters/RunnerMovement.java
@@ -0,0 +1,37 @@
+package badIceCream.controller.game.monsters;
+
+import badIceCream.controller.game.Step;
+import badIceCream.model.Position;
+import badIceCream.model.game.arena.Arena;
+import badIceCream.model.game.elements.monsters.Monster;
+
+import java.io.IOException;
+import java.util.List;
+
+public class RunnerMovement implements Step {
+    @Override
+    public void step(Monster monster, Arena arena) throws IOException {
+        Position pos = getPossible(monster, arena);
+        if (pos != null) moveMonster(monster, pos, arena);
+    }
+
+    private Position getPossible(Monster monster, Arena arena) {
+        List<Position> options = new java.util.ArrayList<>(List.of(new Position[]{monster.getPosition().getDown(), monster.getPosition().getLeft(), monster.getPosition().getUp(), monster.getPosition().getRight()}));
+
+        options.removeIf(pos -> !arena.isEmptyMonsters(pos));
+
+        if (options.isEmpty()) return null;
+
+        java.util.Random random = new java.util.Random();
+        int randomIndex = random.nextInt(options.size());
+
+        return options.get(randomIndex);
+    }
+    @Override
+    public void moveMonster(Monster monster, Position position, Arena arena) {
+        monster.setPosition(position);
+        if (arena.getIceCream().getPosition().equals(position))
+            arena.getIceCream().changeAlive();
+
+    }
+}
