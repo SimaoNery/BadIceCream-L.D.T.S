8230fa478ba015e30dee6c1e62c7bce7b65e0429
SimaoNery <simaonery@gmail.com>
2023-12-04 00:04:46 +0000

    Level Select now works with left and right arrows

--- src/main/java/badIceCream/controller/menu/SelectLevelMenuController.java
+++ src/main/java/badIceCream/controller/menu/SelectLevelMenuController.java
@@ -18,10 +18,10 @@ public class SelectLevelMenuController extends MenuController<SelectLevelMenu> {
     @Override
     public void step(Game game, GUI.ACTION action, long time) throws IOException {
         switch (action) {
-            case UP:
+            case LEFT:
                 getModel().previousEntry();
                 break;
-            case DOWN:
+            case RIGHT:
                 getModel().nextEntry();
                 break;
             case SELECT:
