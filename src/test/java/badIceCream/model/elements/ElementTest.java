6df0da0a2f797098f908e9318b6bd186db653797
nelsonneto21 <nelsonjoneto@gmail.com>
2023-12-10 22:44:10 +0000

    Changed anyInt() to 5 in element testing, because it would interfere with other tests

--- src/test/java/badIceCream/model/elements/ElementTest.java
+++ src/test/java/badIceCream/model/elements/ElementTest.java
@@ -18,67 +18,67 @@ import static org.mockito.ArgumentMatchers.anyInt;
 public class ElementTest {
     @Test
     public void hotFloorGetType() {
-        HotFloor hotFloor = new HotFloor(anyInt(),anyInt());
+        HotFloor hotFloor = new HotFloor(5,5);
         assertEquals(0,hotFloor.getType());
     }
     @Test
     public void iceCreamGetType() {
-        IceCream iceCream = new IceCream(anyInt(),anyInt());
+        IceCream iceCream = new IceCream(5,5);
         assertEquals(1,iceCream.getType());
     }
     @Test
     public void stoneWallGetType() {
-        StoneWall stoneWall = new StoneWall(anyInt(),anyInt());
+        StoneWall stoneWall = new StoneWall(5,5);
         assertEquals(2, stoneWall.getType());
     }
     @Test
     public void iceWallGetType() {
-        IceWall iceWall = new IceWall(anyInt(),anyInt());
+        IceWall iceWall = new IceWall(5,5);
         assertEquals(1, iceWall.getType());
     }
     @Test
     public void wallBreakerGetType() {
-        WallBreakerMonster wallBreakerMonster = new WallBreakerMonster( anyInt(),anyInt());
+        WallBreakerMonster wallBreakerMonster = new WallBreakerMonster( 5,5);
         assertEquals(4, wallBreakerMonster.getType());
     }
     @Test
     public void runnerMonsterGetType() {
-        RunnerMonster runnerMonster = new RunnerMonster(anyInt(),anyInt());
+        RunnerMonster runnerMonster = new RunnerMonster(5,5);
         assertEquals(3, runnerMonster.getType());
     }
     @Test
     public void jumperMonsterGetType() {
-        JumperMonster jumperMonster = new JumperMonster(anyInt(),anyInt());
+        JumperMonster jumperMonster = new JumperMonster(5,5);
         assertEquals(2, jumperMonster.getType());
     }
     @Test
     public void DefaultMonsterGetType() {
-        DefaultMonster defaultMonster = new DefaultMonster(anyInt(),anyInt());
+        DefaultMonster defaultMonster = new DefaultMonster(5,5);
         assertEquals(1, defaultMonster.getType());
     }
     @Test
     public void StrawBerryFruitGetType() {
-        StrawberryFruit strawberryFruit = new StrawberryFruit(anyInt(),anyInt());
+        StrawberryFruit strawberryFruit = new StrawberryFruit(5,5);
         assertEquals(5, strawberryFruit.getType());
     }
     @Test
     public void PineappleFruitGetType() {
-        PineappleFruit pineappleFruit = new PineappleFruit(anyInt(),anyInt());
+        PineappleFruit pineappleFruit = new PineappleFruit(5,5);
         assertEquals(4, pineappleFruit.getType());
     }
     @Test
     public void CherryFruitGetType() {
-        CherryFruit cherryFruit = new CherryFruit(anyInt(),anyInt());
+        CherryFruit cherryFruit = new CherryFruit(5,5);
         assertEquals(3, cherryFruit.getType());
     }
     @Test
     public void BananaFruitGetType() {
-        BananaFruit bananaFruit = new BananaFruit(anyInt(),anyInt());
+        BananaFruit bananaFruit = new BananaFruit(5,5);
         assertEquals(2, bananaFruit.getType());
     }
     @Test
     public void AppleFruitGetType() {
-        AppleFruit appleFruit = new AppleFruit(anyInt(),anyInt());
+        AppleFruit appleFruit = new AppleFruit(5,5);
         assertEquals(1, appleFruit.getType());
     }
 }
