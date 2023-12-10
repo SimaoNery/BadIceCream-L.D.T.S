f138b612ce894f7daf87cfdd5de9b408ff319082
nelsonneto21 <nelsonjoneto@gmail.com>
2023-12-10 18:56:07 +0000

    Corrected the generateNewFruits tests

--- src/test/java/badIceCream/model/game/ArenaTest.java
+++ src/test/java/badIceCream/model/game/ArenaTest.java
@@ -417,7 +417,7 @@ public class ArenaTest {
         private StoneWall stoneWall1, stoneWall2, stoneWall3, stoneWall4,
                 stoneWall5, stoneWall6, stoneWall7, stoneWall8, stoneWall9,
                 stoneWall10, stoneWall11, stoneWall12, stoneWall13, stoneWall14,
-                stoneWall15,  stoneWall16, stoneWall17, stoneWall18, stoneWall19;
+                stoneWall15,  stoneWall16, stoneWall17, stoneWall18, stoneWall19, stoneWall20;
         @BeforeEach
         public void setUp() {
             MockitoAnnotations.openMocks(this);
@@ -427,24 +427,24 @@ public class ArenaTest {
             when(stoneWall3.getPosition()).thenReturn(new Position(2,0));
             when(stoneWall4.getPosition()).thenReturn(new Position(3,0));
             when(stoneWall5.getPosition()).thenReturn(new Position(4,0));
-            when(stoneWall5.getPosition()).thenReturn(new Position(5,0));
-            when(stoneWall6.getPosition()).thenReturn(new Position(0,1));
-            when(stoneWall7.getPosition()).thenReturn(new Position(5,1));
-            when(stoneWall8.getPosition()).thenReturn(new Position(0,2));
-            when(stoneWall9.getPosition()).thenReturn(new Position(5,2));
-            when(stoneWall10.getPosition()).thenReturn(new Position(0,3));
-            when(stoneWall11.getPosition()).thenReturn(new Position(5,3));
-            when(stoneWall12.getPosition()).thenReturn(new Position(0,4));
-            when(stoneWall13.getPosition()).thenReturn(new Position(5,4));
-            when(stoneWall14.getPosition()).thenReturn(new Position(0,5));
-            when(stoneWall15.getPosition()).thenReturn(new Position(1,5));
-            when(stoneWall16.getPosition()).thenReturn(new Position(2,5));
-            when(stoneWall17.getPosition()).thenReturn(new Position(3,5));
-            when(stoneWall18.getPosition()).thenReturn(new Position(4,5));
-            when(stoneWall19.getPosition()).thenReturn(new Position(5,5));
+            when(stoneWall6.getPosition()).thenReturn(new Position(5,0));
+            when(stoneWall7.getPosition()).thenReturn(new Position(0,1));
+            when(stoneWall8.getPosition()).thenReturn(new Position(5,1));
+            when(stoneWall9.getPosition()).thenReturn(new Position(0,2));
+            when(stoneWall10.getPosition()).thenReturn(new Position(5,2));
+            when(stoneWall11.getPosition()).thenReturn(new Position(0,3));
+            when(stoneWall12.getPosition()).thenReturn(new Position(5,3));
+            when(stoneWall13.getPosition()).thenReturn(new Position(0,4));
+            when(stoneWall14.getPosition()).thenReturn(new Position(5,4));
+            when(stoneWall15.getPosition()).thenReturn(new Position(0,5));
+            when(stoneWall16.getPosition()).thenReturn(new Position(1,5));
+            when(stoneWall17.getPosition()).thenReturn(new Position(2,5));
+            when(stoneWall18.getPosition()).thenReturn(new Position(3,5));
+            when(stoneWall19.getPosition()).thenReturn(new Position(4,5));
+            when(stoneWall20.getPosition()).thenReturn(new Position(5,5));
             walls = new ArrayList<>(Arrays.asList(stoneWall1,stoneWall2,stoneWall3,stoneWall4,stoneWall5,
             stoneWall6,stoneWall7, stoneWall8, stoneWall9, stoneWall10, stoneWall11, stoneWall12,
-                    stoneWall13, stoneWall14, stoneWall15, stoneWall16, stoneWall17, stoneWall18, stoneWall19));
+                    stoneWall13, stoneWall14, stoneWall15, stoneWall16, stoneWall17, stoneWall18, stoneWall19, stoneWall20));
             arena.setWalls(walls);
             fruits = new ArrayList<>();
             arena.setFruits(fruits);
