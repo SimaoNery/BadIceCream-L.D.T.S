a4ff260735f80716421283fb87579533d94d6079
Paulo Fidalgo <paulofidalgo@PauloFidalgo.Home>
2023-12-11 22:56:53 +0000

    Updated graphics code from Graphics branch

--- src/main/java/badIceCream/GUI/GameGraphics.java
+++ src/main/java/badIceCream/GUI/GameGraphics.java
@@ -1,14 +1,8 @@
 package badIceCream.GUI;
 
-import badIceCream.model.Position;
 
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
